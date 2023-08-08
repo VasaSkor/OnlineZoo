c997680b3b57cc847d7cc1bf0af2ae392284d0bb
VasaSkor <dzho.ker.700@gmail.com>
2023-08-08 08:35:08 +0300

    refactor: rename className in BAM structure

--- src/components/WatchOnline.tsx
+++ src/components/WatchOnline.tsx
@@ -5,9 +5,9 @@ import '../styles/WatchOnline.scss'
 const WatchOnline = () => {
     return (
         <section className='watch-online'>
-            <div className='watch-online-container'>
-                <div className='watch-online-container-circle'>
-                    <span className='watch-online-container-circle-text'>
+            <div className='watch-online__container'>
+                <div className='watch-online__container__circle'>
+                    <span className='watch-online__container__circle-text'>
                         Watch your <span>favorite</span> animal online
                     </span>
                 </div>

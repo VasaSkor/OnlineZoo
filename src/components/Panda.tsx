506e6f58e93b1ee372729b85a8e4170c9250fdf1
VasaSkor <dzho.ker.700@gmail.com>
2023-08-12 12:32:24 +0300

    feat: add section panda with adaptive

---
+++ src/components/Panda.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Panda.scss'
+
+const Panda = () => {
+    return (
+        <section className='panda'/>
+    );
+};
+
+export default Panda;
--- src/components/pages/Donate.tsx
+++ src/components/pages/Donate.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Panda from "../Panda";
 
 const Donate = () => {
     return (
         <main className='donate'>
-            <h1>Donate Page</h1>
+            <Panda/>
         </main>
     );
 };
